0326c19b6aa5e571f73c7c82a623e81c37af2f76
Sandip Maity <sandipmaity21052003@gmail.com>
2024-10-12 21:01:17 +0530

    ok

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -1,18 +1,19 @@
 import User from "../models/UserSchema.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
+export const signup = async (req, res) => {
+  const { fullName, email, password, mobileNumber, skills } = req.body;
 
-export const signup = async(req, res) => {
-   const { fullName, email, password, mobileNumber, skills} = req.body;
-
-   if (!fullName || !email || !mobileNumber || !skills || !password) {
-    return res.status(400).json({ message: 'Please fill in all fields' });
+  if (!fullName || !email || !mobileNumber || !skills || !password) {
+    return res.status(400).json({ message: "Please fill in all fields" });
   }
 
   try {
-    const existingUser = await User.findOne({ $or: [{ email }, { mobileNumber }] });
-    if(existingUser){
-        return res.status(400).json({ message: 'User already exists' });
+    const existingUser = await User.findOne({
+      $or: [{ email }, { mobileNumber }],
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const newUser = new User({
@@ -25,20 +26,24 @@ export const signup = async(req, res) => {
 
     await newUser.save();
 
-
-    const token = jwt.sign({
+    const token = jwt.sign(
+      {
         userId: newUser._id,
-        email: newUser.email
-    },process.env.JWT_SECRET, {
-        expiresIn: '1h'
-    });
+        email: newUser.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
 
     res.status(201).json({
-        message: 'User created successfully',
-        token,
+      message: "User created successfully",
+      token,
     });
   } catch (error) {
-    res.status(500).json({ message: 'Server error. Please try again later.',error});
+    res
+      .status(500)
+      .json({ message: "Server error. Please try again later.", error });
   }
-   
 };
