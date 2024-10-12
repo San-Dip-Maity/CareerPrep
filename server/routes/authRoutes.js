df4728592d185819aab9f8faa403d91ce7f81fd1
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-10-12 20:19:17 +0530

    auth done

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/signupController.js';
+import { login, logout, signup } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
 
 
 export default router;
