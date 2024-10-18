6496f37635350a7883c304bb8c83bc573b02cfe2
Sandip Maity <sandipmaity21052003@gmail.com>
2024-10-18 21:40:25 +0530

    "https://careerprep.onrender.com" ---backend deploy done

--- client/src/utils/constUtils.js
+++ client/src/utils/constUtils.js
@@ -1,2 +1,2 @@
-export const proxy = "http://localhost:5000/api/";
-export const AUTH_API_END_POINT="http://localhost:5000/api/auth/";
+export const proxy = "https://careerprep.onrender.com/api/";
+export const AUTH_API_END_POINT="https://careerprep.onrender.com/api/auth/";
