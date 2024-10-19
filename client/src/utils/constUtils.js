e7033a98d12f8749bcfd1e2fb4f4e382c96b29eb
Sandip Maity <sandipmaity21052003@gmail.com>
2024-10-19 16:11:31 +0530

    env update

--- client/src/utils/constUtils.js
+++ client/src/utils/constUtils.js
@@ -1,8 +1,8 @@
-// export const proxy = "http://localhost:5000/api/";
-// export const AUTH_API_END_POINT="http://localhost:5000/api/auth/";
+// export const proxy = "http://localhost:5000/api/" || "https://careerprep.onrender.com/api/";
+// export const AUTH_API_END_POINT="http://localhost:5000/api/auth/" || "https://careerprep.onrender.com/api/auth/";
 
 
-export const proxy =  "https://careerprep.onrender.com/api/";
-export const AUTH_API_END_POINT= "https://careerprep.onrender.com/api/auth/";
+export const proxy = process.env.PROXY;
+export const AUTH_API_END_POINT= `${proxy}/auth`;
 
 
