2d0d34174280f058d88c2c59ec0fb7af9df97d3b
Sandip Maity <sandipmaity21052003@gmail.com>
2024-10-20 14:06:52 +0530

    ok7

--- client/src/utils/constUtils.js
+++ client/src/utils/constUtils.js
@@ -1,4 +1,4 @@
 export const proxy =
-  "https://careerprep-production.up.railway.app/api";
+  "https://careerprep-production.up.railway.app/api/";
 export const AUTH_API_END_POINT =
-  "https://careerprep-production.up.railway.app/api/auth";
+  "https://careerprep-production.up.railway.app/api/auth/";
