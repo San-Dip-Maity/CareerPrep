4085991563501f3bde8c26c415d9e16a7acf7c9c
Sandip Maity <sandipmaity21052003@gmail.com>
2024-10-20 18:54:04 +0530

    api ta jeta comment a6e ota uncomment kore nibi

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req,res)=>{
-  return res.json({message:"Hello"})
+  return res.send("AppName: CareerPrep")
 })
 app.use("/api/auth", authRoutes);
 app.use("/api/job", jobRoutes);
