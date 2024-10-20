d3ef82c78cd9d09b962afe735ebb8961380cc095
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-10-20 18:25:15 +0530

    bug fix and some changes in job controller

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/isAuthenticated.js";
-import { adminCheckJobCount, getJobs, postJobs } from "../controllers/jobController.js";
+import { adminCheckJobCount, getJobById, getJobs, postJobs } from "../controllers/jobController.js";
 
 const router = express.Router();
 
 router.post("/create",protectRoute,postJobs);
 router.get("/get",protectRoute,getJobs);
-router.post("/getadminJobs",protectRoute,adminCheckJobCount)
+router.post("/getadminJobs",protectRoute,adminCheckJobCount);
+router.get("/getJobs/:id",protectRoute,getJobById);
 
 export default router;
