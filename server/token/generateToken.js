1cafaa832407e06d7c87d08c75cd6c9eece54549
Sandip Maity <sandipmaity21052003@gmail.com>
2024-10-24 17:25:38 +0530

    ok

--- server/token/generateToken.js
+++ server/token/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId,res) =>{
         maxAge: 15 * 24 * 60 * 60 *1000,
         httpOnly: true,
         sameSite: "strict",
-        secure : process.env.NODE_ENV !== "devolopment",
+        secure : process.env.NODE_ENV !== "development",
     });
 };
 
